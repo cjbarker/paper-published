3ab4211a4929e8d6695ceae78e6822500abacdfc
CJ Barker <cjbarker@gmail.com>
2020-08-26 14:08:11 -0700

    Initial test created

--- tests/test_pp.py
+++ tests/test_pp.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
+import pp
+import pytest
+import sys
+
 def test_is_valid_file():
-    pass
+    test_file = sys.argv[0]
+    assert pp.is_valid_file("foo") is False
+    assert pp.is_valid_file(test_file) is True
+
